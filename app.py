77b06a4c6d66f5791621824543f31cfa211df9cc
ROAG97 <robert.agnarp@gmail.com>
2026-09-05 20:29:22 +0200

    lagt till visa värn knapp

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, abort
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +14,44 @@ def geojson():
     return send_file(
         "data/BunkerLayer.geojson",
         mimetype="application/geo+json"
+    )
+
+
+@app.route("/varn/<nr>")
+def varn(nr):
+
+    # Läs kartdatan
+    with open("data/BunkerLayer.geojson", "r", encoding="utf-8") as file:
+        geojson_data = json.load(file)
+
+    vald_varn = None
+
+    for feature in geojson_data["features"]:
+        properties = feature["properties"]
+
+        if str(properties.get("Nr")) == str(nr):
+            vald_varn = feature
+            break
+
+
+    # Om numret inte finns i GeoJSON
+    if vald_varn is None:
+        abort(404)
+
+
+    # Läs den fördjupade informationen
+    with open("data/varn.json", "r", encoding="utf-8") as file:
+        varn_data = json.load(file)
+
+
+    # Hämta detaljinfo för exempelvis "683"
+    detaljinfo = varn_data.get(str(nr), {})
+
+
+    return render_template(
+        "varn.html",
+        varn=vald_varn,
+        detaljinfo=detaljinfo
     )
 
 
